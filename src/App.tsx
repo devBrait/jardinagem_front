d33fffff994556c5e1d17b4ab17304ba4bac83ee
brait <lorimiervb@gmail.com>
2024-09-10 22:50:41 -0300

    material ui adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Button from "@mui/material/Button"
+
+
 function App() {
   return (
     <>
      <h1 className="text-2xl text-black font-bold mt-5">
         Olá UmEntrePosto
      </h1>
+     <Button variant="contained" className="bg-black text-1xl">Material Ui Button</Button>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { StyledEngineProvider } from '@mui/material'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
   </StrictMode>,
 )
